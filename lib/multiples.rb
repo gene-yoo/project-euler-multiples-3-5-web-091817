b3ec8a32e757384e271f658755136acd48398f8c
gene-yoo <gene.y.yoo@gmail.com>
2017-09-29 14:58:01 -0400

    Lab complete.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  array = (1...limit).to_a
+  array.select {|x| x % 3 == 0 || x % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    array = (1...(self.limit)).to_a
+    array.select {|x| x % 3 == 0 || x % 5 == 0}
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+end
